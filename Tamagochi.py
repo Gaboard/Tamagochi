3687f606c697bd415bac41a3a165de579f15f989
Gabriel Valencia <gabovalencia2009@gmail.com>
2023-10-10 23:25:53 -0600

    Creando el archivo 'Tamagochi.py' y el menú principal

---
+++ Tamagochi.py
@@ -0,0 +1,14 @@
+# Creación del menú de opcioones para tamagochi
+import os
+runProgram = True
+
+#Función para mostrar las opciones del menú
+def showMenuOptions():
+    print("¿Qué actividad desas que realize tu tamagochi?")
+    print("")
+    print("1) Jugar")
+    print("2) Comer")
+    print("3) Dormir")
+    print("4) Ver estado")
+    print("5) Salir\n")
+
