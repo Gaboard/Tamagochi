3bf4063269e5792bbf5c0444a6464fc3bae6c249
Gabriel Valencia <gabovalencia2009@gmail.com>
2023-10-14 16:09:12 -0600

    Modificando las opciones de los submenús

--- Tamagochi.py
+++ Tamagochi.py
@@ -39,16 +39,16 @@ def menu():
 # Función para el submenú JUGAR
 def play():
     print("¿A qué deseas jugar?\n")
-    print("1) Jugar al parque.")
-    print("2) Jugar con un juguete.")
+    print("1) Jugar al frifayer.")
+    print("2) Jugar a romper PR en press banca.")
     print("3) No jugar.")
     while True:
         opc = int(input("Ingresa una opción para jugar (1, 2 o 3): "))
         match opc:
             case 1:
-                print("Jugemos en el parque.")
+                print("Jugar al frifayer te hará gritar como niño rata.")
             case 2:
-                print("Jugemos con un juguete.")
+                print("Jugar a romper tu PR en press banca te podnrá mamadisimo hdsptm.")
             case 3:
                 print("Jugaremos más tarde.")
                 break
@@ -58,35 +58,35 @@ def play():
 #Función para el submenú COMER
 def comer():
     print("¿Qué deseas jugar?\n")
-    print("1) Comer fruta.")
-    print("2) Comer comida rápida.")
+    print("1) Comer prote + preworkout.")
+    print("2) Comer atún + plátano.")
     print("3) No comer.")
     while True:
         opc = int(input("Ingresa una opción para jugar (1, 2 o 3): "))
         match opc:
             case 1:
-                print("Comer furta te volverá más fuerte.")
+                print("Comer la prote + el preworkput te pondrá mamadismo hdsptm.")
             case 2:
-                print("La comida rápida puede ser deliciosa, pero no es buena para tu salud.")
+                print("Comer atún + plátano también te pondra mamadismo hdsptm pero sin taquicardias.")
             case 3:
                 print("Comeremos algo más tarde.")
                 break
             case _: 
                 print("ERROR. Opción no válida. Ingresa una opción correcta (1, 2, o 3): ")
 
-#Función para el submenú 
+#Función para el submenú DORMIR
 def dormir():
     print("¿Qué tanto tiempo deseas dormir?\n")
-    print("1) Dormir una siesta.")
-    print("2) Dormir toda la noche.")
+    print("1) Dormir 1 hora (sin poner alarma).")
+    print("2) Dormir toda la noche (sin soñar con ella).")
     print("3) No dormir.")
     while True:
         opc = int(input("Ingresa una opción para jugar (1, 2 o 3): "))
         match opc:
             case 1:
-                print("Dormir toda la noche te ayudará a recuperar tu energía por completo")
+                print("Dormir 1 hora sin alarma hará que duermas alrededor de 3 horas xd")
             case 2:
-                print("Dormir una siesta te ayudará a recuperar 20 puntos de energía.")
+                print("Dormir toda la noche sin pensar en ella hará que recuperes toda tu energía")
             case 3:
                 print("Dormiremos más tarde.")
                 break
